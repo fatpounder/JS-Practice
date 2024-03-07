0742e4f9ee320ecfd1e9c97038605516abf13185
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-03-07 14:40:17 -0600

    changes

--- practice1.js
+++ practice1.js
@@ -1,6 +1,8 @@
-let n = 6
-let total = 0
-for (let i=1; i<=n;i++) {
-    total+=i;
+function addUpTo(n) {
+    let n = 6
+    let total = 0
+    for (let i=1; i<=n;i++) {
+        total+=i;
+    }
+    console.log(total);
 }
-console.log(total);
