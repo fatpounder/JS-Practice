e984e57fa9ef752770978d8bebce20bd088cf560
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-03-09 21:27:06 -0600

    fn

---
+++ practice2.js
@@ -0,0 +1,13 @@
+function memoize(fn) {
+    let map = {};
+    return function(...args) {
+        const s = args.toString();
+        if (s in map) {
+            return map[s];
+        } else {
+            let res = fn(...args);
+            map[s]= res;
+            return res;
+        }
+    }
+}
